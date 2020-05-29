9ecca8c3c770df9131bffdd5336533d020ce786c
enaix <enaix@protonmail.com>
2020-05-29 16:55:46 +0300

    Add error toasts

--- server/dashboard/js/main.js
+++ server/dashboard/js/main.js
@@ -1,3 +1,13 @@
+Vue.use(Toasted, {
+  action : {
+    text: '●',
+    onClick : (e, toastObject) => {
+      toastObject.goAway(0)
+    },
+  },
+  duration: 10000,
+})
+
 var app = new Vue({
   el: '#dashboard',
   data: {
@@ -9,6 +19,8 @@ var app = new Vue({
     warningsCount: 3,
     errors: ['[!] WorkUnit not found', '[3] Failed to reload WU'],
     errorsCount: 2,
+    errorIcon: '<svg class="bi bi-asterisk c1-fg" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 0a1 1 0 0 1 1 1v5.268l4.562-2.634a1 1 0 1 1 1 1.732L10 8l4.562 2.634a1 1 0 1 1-1 1.732L9 9.732V15a1 1 0 1 1-2 0V9.732l-4.562 2.634a1 1 0 1 1-1-1.732L6 8 1.438 5.366a1 1 0 0 1 1-1.732L7 6.268V1a1 1 0 0 1 1-1z"/></svg>',
+    warningIcon: '<svg class="bi bi-asterisk c3-fg" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 0a1 1 0 0 1 1 1v5.268l4.562-2.634a1 1 0 1 1 1 1.732L10 8l4.562 2.634a1 1 0 1 1-1 1.732L9 9.732V15a1 1 0 1 1-2 0V9.732l-4.562 2.634a1 1 0 1 1-1-1.732L6 8 1.438 5.366a1 1 0 0 1 1-1.732L7 6.268V1a1 1 0 0 1 1-1z"/></svg>',
   },
   methods: {
     isEven: function (a) {
@@ -17,6 +29,12 @@ var app = new Vue({
     newWarning: function (warn) {
       this.warnings.push(warn)
       this.warningsCount = this.warnings.length
-    }
+      Vue.toasted.show(this.warningIcon + " " + warn)
+    },
+    newError: function (err) {
+      this.errors.push(err)
+      this.errorsCount = this.errors.length
+      Vue.toasted.show(this.errorIcon + " " + err)
+    },
   }
 })
