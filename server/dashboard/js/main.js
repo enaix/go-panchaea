dfe3ff6af9d9f3dc8b8b5f7a020a928603ac8c08
enaix <enaix@protonmail.com>
2020-05-29 15:58:17 +0300

    Add errors management

--- server/dashboard/js/main.js
+++ server/dashboard/js/main.js
@@ -5,8 +5,18 @@ var app = new Vue({
     statusWrapperColor: 'c11-bg c0-fg',
     isWarningsCollapsed: true,
     isErrorsCollapsed: true,
-    warnings: [],
-    warningsCount: 0,
-    errors: [],
+    warnings: ['[*] Cannot find config file panchaea-client.json', '[1] Thread 3 is stuck', '[1] Thread 2 is stuck'],
+    warningsCount: 3,
+    errors: ['[!] WorkUnit not found', '[3] Failed to reload WU'],
+    errorsCount: 2,
+  },
+  methods: {
+    isEven: function (a) {
+      return a % 2 == 0
+    },
+    newWarning: function (warn) {
+      this.warnings.push(warn)
+      this.warningsCount = this.warnings.length
+    }
   }
 })
